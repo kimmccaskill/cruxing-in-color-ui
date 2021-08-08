db63f33e11eaf4e6c2431ccf3e692fb8a2916618
Kim McCaskill <kimmccaskill92@gmail.com>
2021-08-08 00:33:54 -0600

    Fix styling for image with desc box

--- src/components/ImageWithDescBox.jsx
+++ src/components/ImageWithDescBox.jsx
@@ -1,64 +1,101 @@
-import React, { Fragment } from 'react';
-import { Container, Typography, Paper, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Fragment } from "react";
+import { Container, Typography, Paper, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // width: '100%',
-    // paddingRight: theme.spacing(10),
     paddingLeft: theme.spacing(20),
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+      paddingLeft: 0,
+      flexDirection: "column",
+    },
   },
   root2: {
-    // width: '100%',
     paddingRight: theme.spacing(20),
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-    // paddingLeft: theme.spacing(10),
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'row-reverse',
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row-reverse",
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+      paddingRight: 0,
+      flexDirection: "column-reverse",
+    },
   },
   img: {
-    width: '60%',
-    objectFit: 'cover',
-    maxHeight: '500px',
+    width: "60%",
+    objectFit: "cover",
+    maxHeight: "500px",
+    [theme.breakpoints.down("sm")]: {
+      width: "70%",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
   },
   descriptionBox: {
-    backgroundColor: '#F0EEEE',
+    backgroundColor: "#F0EEEE",
     right: theme.spacing(20),
-    width: '40%',
-    height: 'fit-content',
-    position: 'absolute',
+    width: "40%",
+    height: "fit-content",
     padding: theme.spacing(4),
-    display: "flex",
-    flexDirection: 'column'
+    position: "absolute",
+    [theme.breakpoints.down("sm")]: {
+      left: "auto",
+      backgroundColor: "inherit",
+      padding: theme.spacing(0),
+      paddingTop: theme.spacing(2),
+      width: "70%",
+      position: "inherit",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
   },
   descriptionBox2: {
-    backgroundColor: '#F0EEEE',
+    backgroundColor: "#F0EEEE",
     left: theme.spacing(20),
-    width: '40%',
-    height: 'fit-content',
-    position: 'absolute',
+    width: "40%",
+    height: "fit-content",
     padding: theme.spacing(4),
+    position: "absolute",
+    [theme.breakpoints.down("sm")]: {
+      left: "auto",
+      backgroundColor: "inherit",
+      padding: theme.spacing(0),
+      paddingTop: theme.spacing(2),
+      width: "70%",
+      position: "inherit",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
   },
   button: {
     marginTop: theme.spacing(3),
-    width: 'fit-content'
-  }
+    width: "fit-content",
+  },
+  testroot: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 }));
 
 const ImageWithDescBox = (props) => {
   const {
     src,
-    alt = '',
-    title = '',
-    description = '',
+    alt = "",
+    title = "",
+    description = "",
     description2 = null,
-    imageSide = 'left',
+    imageSide = "left",
     buttonLink = null,
     buttonTitle = null,
   } = props;
@@ -70,15 +107,21 @@ const ImageWithDescBox = (props) => {
         {title}
       </Typography>
       <Typography variant="body1">{description}</Typography>
-      {description2 && (
-        <Typography variant="body1">{description}</Typography>
-      )
-      }
-      {buttonTitle && buttonLink && (<Button className={classes.button} target="_blank" href={buttonLink} disableElevation variant="contained">{buttonTitle}</Button>)}
+      {description2 && <Typography variant="body1">{description}</Typography>}
+      {buttonTitle && buttonLink && (
+        <Button
+          className={classes.button}
+          target="_blank"
+          href={buttonLink}
+          disableElevation
+          variant="contained"
+        >
+          {buttonTitle}
+        </Button>
+      )}
     </Fragment>
-
-  )
-  return imageSide === 'left' ? (
+  );
+  return imageSide === "left" ? (
     <div className={classes.root}>
       <img className={classes.img} src={src} alt={alt} />
       <Paper className={classes.descriptionBox} elevation={0} square={true}>
