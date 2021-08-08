af3b8f051dcdb1302afb08b43f57ff9b63bfad57
Kim McCaskill <kimmccaskill92@gmail.com>
2021-08-08 00:33:08 -0600

    Update Widget

---
+++ src/components/EmbedSocialWidjet.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class EmbedSocialWidget extends React.Component {
+  render() {
+    return (
+      <div
+        class="embedsocial-hashtag"
+        data-ref="a7373a0af5a016197211f0734b23cd20bb64b0cf"
+      ></div>
+    );
+  }
+
+  componentDidMount() {
+    (function (d, s, id) {
+      var js;
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://embedsocial.com/cdn/ht.js";
+      d.getElementsByTagName("head")[0].appendChild(js);
+    })(document, "script", "EmbedSocialHashtagScript");
+  }
+}
+
+export default EmbedSocialWidget;
