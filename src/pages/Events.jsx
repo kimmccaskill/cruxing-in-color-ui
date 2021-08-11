fa31df4d1df8ce2ab10bed32f9eddf201897a4db
Kim McCaskill <kimm@4thdownsoftware.com>
2021-08-11 15:58:00 -0600

    Fix for deploymeny

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, Fragment } from "react";
-import axios from "axios";
-import { DateTime } from "luxon";
-import { Container, Typography, Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import HeaderImage from "../components/HeaderImage";
-import Event from "../components/Event";
-import image from "../images/events-img.png";
+import React, {useState, useEffect, Fragment} from 'react';
+import axios from 'axios';
+import {DateTime} from 'luxon';
+import {Container, Typography, Paper} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import HeaderImage from '../components/HeaderImage';
+import Event from '../components/Event';
+import image from '../images/events-img.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -13,16 +13,16 @@ const useStyles = makeStyles((theme) => ({
   title: {
     marginTop: theme.spacing(6),
     marginBottom: theme.spacing(6),
-    [theme.breakpoints.down("xs")]: {
+    [theme.breakpoints.down('xs')]: {
       marginTop: theme.spacing(3),
       marginBottom: theme.spacing(3),
     },
   },
   eventsContainer: {
-    display: "flex",
-    flexWrap: "wrap",
-    [theme.breakpoints.down("xs")]: {
-      flexDirection: "column",
+    display: 'flex',
+    flexWrap: 'wrap',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
       paddingLeft: theme.spacing(2),
     },
   },
@@ -34,7 +34,7 @@ const Events = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://strapi-cic.herokuapp.com/events");
+      const result = await axios('https://strapi-cic.herokuapp.com/events');
 
       setEvents(result.data);
     };
@@ -52,7 +52,7 @@ const Events = () => {
 
     sortedEvents.forEach((event) => {
       const dt = DateTime.fromISO(event.date);
-      if (today.startOf("day") > dt.startOf("day")) {
+      if (today.startOf('day') > dt.startOf('day')) {
         pastEvents.push(event);
       } else {
         futureEvents.push(event);
@@ -80,18 +80,20 @@ const Events = () => {
           Past Events
         </Typography>
         <div className={classes.eventsContainer}>
-          {pastEvents.map((event) => {
-            const date = DateTime.fromISO(event.date);
-            return (
-              <Event
-                date={date.toLocaleString(DateTime.DATE_FULL)}
-                title={event.title}
-                location={event.location}
-                buttonTitle={event.buttonTitle}
-                buttonLink={event.buttonLink}
-              />
-            );
-          })}
+          {pastEvents
+            .map((event) => {
+              const date = DateTime.fromISO(event.date);
+              return (
+                <Event
+                  date={date.toLocaleString(DateTime.DATE_FULL)}
+                  title={event.title}
+                  location={event.location}
+                  buttonTitle={event.buttonTitle}
+                  buttonLink={event.buttonLink}
+                />
+              );
+            })
+            .reverse()}
         </div>
       </Container>
     );
