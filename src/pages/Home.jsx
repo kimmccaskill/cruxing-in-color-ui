3d3d4fe0802bb74c7a3cb3527b7367e438876987
Kim McCaskill <kimmccaskill92@gmail.com>
2021-09-21 17:19:07 -0600

    homepage and about changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,63 @@
-import React from 'react';
-import {Container, Typography} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
-import HeaderImage from '../components/HeaderImage';
-import groupImg from '../images/home-img.png';
-import EmbedSocialWidget from '../components/EmbedSocialWidget';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import HeaderImage from "../components/HeaderImage";
+import groupImg from "../images/home-img.png";
+import EmbedSocialWidget from "../components/EmbedSocialWidget";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: '80vh',
+    minHeight: "80vh",
   },
   mission: {},
   textBlock: {
     marginTop: theme.spacing(6),
     marginBottom: theme.spacing(6),
   },
+  instagram: {
+    marginTop: theme.spacing(4),
+  },
 }));
 
 const Home = () => {
+  const [homepage, setHomepage] = useState({});
   const classes = useStyles();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("https://strapi-cic.herokuapp.com/homepages");
+
+      setHomepage(result.data[0]);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderHeaderImage = () => (
+    <HeaderImage src={homepage.img || groupImg} alt="Group of CiC members" />
+  );
+
+  const renderMission = () => (
+    <Container className={classes.mission} maxWidth="md">
+      <Typography className={classes.textBlock} variant="h5" align="center">
+        {homepage.textBlock1}
+      </Typography>
+      {homepage.textBlock2 && (
+        <Typography className={classes.textBlock} variant="h5" align="center">
+          {homepage.textBlock2}
+        </Typography>
+      )}
+    </Container>
+  );
+
   return (
     <Container className={classes.root} maxWidth={false}>
-      <HeaderImage src={groupImg} alt="Group of CiC members" />
-      <Container className={classes.mission} maxWidth="md">
-        <Typography className={classes.textBlock} variant="h5" align="center">
-          Our mission is to connect, educate, and inspire self-identified
-          climbers of color in the Denver area by organizing meetups,
-          scholarships and by offering free mentorship and access to gear.
-        </Typography>
-        <Typography className={classes.textBlock} variant="h5" align="center">
-          We aim to foster representation at the crag and gym and break down
-          barriers that systemically keep people of color out of these spaces.
-        </Typography>
-      </Container>
-      <EmbedSocialWidget />
+      {renderHeaderImage()}
+      {homepage.textBlock1 && renderMission()}
+      <div className={classes.instagram}>
+        <EmbedSocialWidget />
+      </div>
     </Container>
   );
 };
