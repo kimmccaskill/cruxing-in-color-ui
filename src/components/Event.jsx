4ea66387b0cd164ce20956e74f8758b0aeb2a701
Kim McCaskill <kimmccaskill92@gmail.com>
2022-04-30 03:03:56 -0600

    Update events

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import {Container, Typography, Box, Button} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import React from "react";
+import { Container, Typography, Box, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(6),
     marginBottom: theme.spacing(6),
-    width: '25%',
-    [theme.breakpoints.down('sm')]: {
-      width: '50%',
-    },
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
+    width: "23%",
+    marginRight: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    [theme.breakpoints.down("sm")]: {
+      width: "50%",
       marginTop: theme.spacing(3),
       marginBottom: theme.spacing(3),
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
     },
   },
   details: {
@@ -30,7 +32,7 @@ const Event = (props) => {
     time,
     title,
     location,
-    description = '',
+    description = "",
     buttonTitle = null,
     buttonLink = null,
   } = props;
@@ -39,9 +41,23 @@ const Event = (props) => {
     <div className={classes.root}>
       <Typography variant="body1">
         <Box fontWeight="fontWeightBold">{date}</Box>
+      </Typography>
+      <Typography variant="subtitle2">
         <Box className={classes.details}>{time}</Box>
-        <Box className={classes.details}>{title}</Box>
+      </Typography>
+      <Typography variant="body1">
+        <Box
+          className={classes.details}
+          style={{ textDecoration: "underline" }}
+          fontWeight={500}
+        >
+          {title}
+        </Box>
+
         <Box className={classes.details}>{location}</Box>
+      </Typography>
+      {description && <div>_</div>}
+      <Typography variant="body1">
         <Box className={classes.details}>{description}</Box>
       </Typography>
       {buttonTitle && buttonLink && (
